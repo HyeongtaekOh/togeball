6262918bde211ac8dcc3e0ebe3e9f073746eca7e
오형택 <oh052679@gmail.com>
2024-01-18 14:50:30 +0900

    Initial Commit

---
+++ backend/src/test/java/com/ssafy/togeball/TogeballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.togeball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogeballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
