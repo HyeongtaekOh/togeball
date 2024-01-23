c1e2e597a3114b7d7c2d8cfa1039de626e9805b5
조권호 <whrnsgjh12@naver.com>
2024-01-23 16:32:34 +0900

    Feat: Add ProfileCard Component

---
+++ frontend/src/model/types/Participands.d.ts
@@ -0,0 +1,11 @@
+export interface ParticipantsType {
+    userId?: number,
+    nickname?: string,
+    age?: number,
+    gender?: string,
+    profileImg?: string,
+    hashtags?: Array<string>
+  }
+  
+  
+  
--- frontend/src/pages/dev/index.tsx
+++ frontend/src/pages/dev/index.tsx
@@ -1,8 +1,9 @@
 import { InputBox,SignButton, Button, Select, Title, KakaoIcon, 
-  NaverIcon, MainLayout, OpenChatCard, BoardList, ChatList, Tag, TagBtn, TagList, HomeLayout } from '../../components'
+  NaverIcon, MainLayout, OpenChatCard, BoardList, ChatList, Tag, TagBtn, TagList, ProfileCard, HomeLayout } from '../../components'
 import { useNavigate } from 'react-router-dom'
 import { useCallback, useState } from 'react'
 import { styled } from 'styled-components'
+import { ParticipantsType } from '@/model/types/Participands'
 
 
 const InputWrapper = styled.div`
@@ -58,14 +59,28 @@ const Dev = () => {
         { title: '# 키움' },
         { title: '# 팀무관' }
 ])
+const [ participant, setParticipant ] = useState<ParticipantsType>(
+  {
+    userId: 1,
+    nickname: '이운재',
+    age: 32,
+    gender: '남성',
+    profileImg: 'https://i.namu.wiki/i/qA822LZ7zUrIq-AH8Q7IhXiCv_YyBYnZLwGYosmEpAz25gvsgBIUusUXsTJqxKhbSsBbYvSeqavdNmHc06s0FQ.webp',
+    hashtags: ['#LG', '#KT', '#SSG', '#NC', '#LG', '#KT', '#SSG', '#NC','#LG', '#KT', '#SSG', '#NC' ]
+  }
+)
 
 
   return (
     <MainLayout>
+
+      <ProfileCard participant = { participant }></ProfileCard>
+
       <TagList tags = { tags } />
       <TagList tags = { tagsRemove } isRemove= { true } />
       <TagList tags = { tagsRemove } bgColor= '#FBD14B' isRemove= { true } />
 
+      
       <BoardList type='main' title='첫번째 제목' createdAt='10:10:10' user='이운재' logo='https://t1.daumcdn.net/cfile/tistory/2647254A5889D6BA03'></BoardList>
       <ChatList type='main' title='1/30 두산vs기아 18:00' mbti='#estp' numberofuser='3/4명' content='어디야?' ></ChatList>
 
@@ -76,7 +91,6 @@ const Dev = () => {
       <SignButton>로그인</SignButton>
 
       <OpenChatCard children='하이하이하이' child='바이바이바이' />
-      </HomeLayout>
     </MainLayout>
   )
 }
