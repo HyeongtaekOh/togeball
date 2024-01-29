131df294b5fd9e41aa231db8bce137d1d589b48b
조아영 <aycho3030@gmail.com>
2024-01-29 15:21:24 +0900

    feat/league API

--- backend/src/main/java/com/ssafy/togeball/domain/league/service/LeagueServiceImpl.java
+++ backend/src/main/java/com/ssafy/togeball/domain/league/service/LeagueServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.togeball.domain.league.service;
 
 import com.ssafy.togeball.domain.league.dto.ClubResponse;
 import com.ssafy.togeball.domain.league.dto.GameResponse;
+import com.ssafy.togeball.domain.league.entity.Club;
 import com.ssafy.togeball.domain.league.entity.Game;
 import com.ssafy.togeball.domain.league.repository.ClubRepository;
 import com.ssafy.togeball.domain.league.repository.ClubStadiumRepository;
@@ -24,23 +25,48 @@ public class LeagueServiceImpl implements LeagueService {
     private final GameRepository gameRepository;
 
 
+//    @Override
+//    public List<Integer> findAll() {
+//        return gameRepository.findAll().stream().map(g -> g.getId()).toList();
+//    }
+
     public List<GameResponse> findBySponsorName(String sponsorName) {
         List<Game> games = gameRepository.findBySponsorName(sponsorName);
-        return convertToDtoList(games);
+        return convertToGameResponseList(games);
     }
 
     public List<GameResponse> findByDate(Date startDate, Date endDate) {
         List<Game> games = gameRepository.findByDate(startDate, endDate);
-        return convertToDtoList(games);
+        return convertToGameResponseList(games);
     }
 
-    public List<GameResponse> convertToDtoList(List<Game> games) {
+    public List<GameResponse> findBySponsorNameAndDate(Date date, String sponsorName) {
+        List<Game> games = gameRepository.findBySponsorNameAndDate(date, sponsorName);
+        return convertToGameResponseList(games);
+    }
+
+    public List<ClubResponse> sortByRanking() {
+        List<Club> clubs = clubRepository.findAllByOrderByRankingAsc();
+        return convertToClubResponseList(clubs);
+    }
+
+    public List<GameResponse> convertToGameResponseList(List<Game> games) {
         List<GameResponse> gameResponses = new ArrayList<>();
-        for (int i=0; i<games.size(); i++) {
-            GameResponse gameResponse = null;
-            gameResponse = gameResponse.of(games.get(i));
+        for (int i = 0; i < games.size(); i++) {
+            Game game = games.get(i);
+            GameResponse gameResponse = GameResponse.of(game);
             gameResponses.add(gameResponse);
         }
         return gameResponses;
+    }
+
+    public List<ClubResponse> convertToClubResponseList(List<Club> clubs) {
+        List<ClubResponse> clubResponses = new ArrayList<>();
+        for (int i = 0; i < clubs.size(); i++) {
+            Club club = clubs.get(i);
+            ClubResponse clubResponse = ClubResponse.of(club);
+            clubResponses.add(clubResponse);
+        }
+        return clubResponses;
     }
 }
