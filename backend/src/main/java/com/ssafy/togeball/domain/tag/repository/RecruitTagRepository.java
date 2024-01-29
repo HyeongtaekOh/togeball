4ca95eea025344d74894498c15eb779694df80e1
양유경 <gosjorb@gmail.com>
2024-01-29 13:01:00 +0900

    Feat : Tag Service 및 Test Code 작성

--- backend/src/main/java/com/ssafy/togeball/domain/tag/repository/RecruitTagRepository.java
+++ backend/src/main/java/com/ssafy/togeball/domain/tag/repository/RecruitTagRepository.java
@@ -2,10 +2,17 @@ package com.ssafy.togeball.domain.tag.repository;
 
 import com.ssafy.togeball.domain.tag.entity.RecruitTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecruitTagRepository extends JpaRepository<RecruitTag, Integer> {
 
+    @Query("select rt from RecruitTag rt where rt.recruitChatroom.id = :chatroomId and rt.tag.id = :tagId")
     Optional<RecruitTag> findByRecruitChatroomIdAndTagId(Integer chatroomId, Integer tagId);
+
+    List<RecruitTag> findAllByRecruitChatroomId(Integer chatroomId);
+
+    void deleteByRecruitChatroomId(Integer chatroomId);
 }
--- backend/src/main/java/com/ssafy/togeball/domain/tag/repository/UserTagRepository.java
+++ backend/src/main/java/com/ssafy/togeball/domain/tag/repository/UserTagRepository.java
@@ -2,15 +2,23 @@ package com.ssafy.togeball.domain.tag.repository;
 
 import com.ssafy.togeball.domain.tag.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserTagRepository extends JpaRepository<UserTag, Integer> {
 
+    @Query("select ut from UserTag ut where ut.tag.id = :tagId and ut.user.id = :userId")
     Optional<UserTag> findByTagIdAndUserId(Integer tagId, Integer userId);
 
     List<UserTag> findByUserId(Integer userId);
 
     List<UserTag> findByTagId(Integer tagId);
+
+    @Query("select ut from UserTag ut where ut.user.id in :userIds")
+    List<UserTag> findByUserIdIn(Set<Integer> userIds);
+
+    void deleteByUserId(Integer userId);
 }
