addda639e4f9b6d2a8031814921ab991e06ed713
윤진아 <chris2761@naver.com>
2024-01-31 17:39:53 +0900

    Refector : modify axios and game type

--- frontend/src/types/Game.d.ts
+++ frontend/src/types/Game.d.ts
@@ -1,11 +1,11 @@
 export interface GameType {
-  gameId?: number,
-  chatroomId?: number,
+  id?: number,
+  // chatroomId?: number,
   datetime?: string,
   homeClubName?: string,
   awayClubName?: string,
   stadiumName?: string,
-  logo?: string,
+  // logo?: string,
 }
 // name => id
 // join해서 넘기기 때문에 타입이 변경될 가능성이 있음.
