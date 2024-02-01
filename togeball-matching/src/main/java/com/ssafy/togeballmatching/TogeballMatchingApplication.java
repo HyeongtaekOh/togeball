bca2580f78621afe816c4c1f70931cdb40026acc
HyeongtaekOh <oh052679@google.com>
2024-02-01 17:46:30 +0900

    Feat: 매칭 서버 프로젝트 추가

---
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/TogeballMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.togeballmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogeballMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogeballMatchingApplication.class, args);
+    }
+
+}
