a851d68c2ae6ad97046c72bdb7cc9d8f04ce6601
오형택 <oh052679@gmail.com>
2024-02-02 02:08:21 +0900

    Feat: WebSocket 세션 관리 기능 추가

---
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.togeballmatching.config;
+
+import com.ssafy.togeballmatching.interceptor.AuthHandshakeInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final CustomWebSocketHandler customWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "/matching")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new AuthHandshakeInterceptor());
+    }
+}
