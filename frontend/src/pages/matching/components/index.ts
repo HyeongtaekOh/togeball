c2cbe1e7231d56099d005487096b1923f88762fa
이운재 <hil00077@khu.ac.kr>
2024-02-01 16:49:16 +0900

    Fix: matchingpage

---
+++ frontend/src/pages/matching/components/index.ts
@@ -0,0 +1,4 @@
+import MatchingQueue from "./MatchingQueue"
+import Timer from "./Timer"
+
+export { MatchingQueue, Timer }
--- frontend/src/pages/matching/index.tsx
+++ frontend/src/pages/matching/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Stomp from 'stompjs';
-import MatchingQueueBubbleChart from './components';
+import   { MatchingQueue, Timer }   from './components';
+import { Title } from 'src/components'
 
 // const WebSocketComponent = () => {
 //   const [matchingQueue, setMatchingQueue] = useState([]);
@@ -47,28 +48,35 @@ import MatchingQueueBubbleChart from './components';
 
 
 
-const TemporaryParentComponent: React.FC = () => {
-  const [matchingData, setMatchingData] = useState({
-    hashtags: ['tag1', 'tag2', 'tag3'],
+const Matching: React.FC = () => {
+  // 매칭에 올라온 큐를 다음과 같이 바꿔야함.
+  const [ matchingData, setMatchingData ] = useState({
+    hashtags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6'],
     counts: {
       tag1: 10,
       tag2: 5,
       tag3: 8,
+      tag4: 12,
+      tag5: 15,
+      tag6: 20,
     },
   });
 
   useEffect(() => {
     // 매 3초마다 임시 데이터를 업데이트하여 컴포넌트를 리렌더링
     const intervalId = setInterval(() => {
-      setMatchingData((prevData) => ({
+      setMatchingData(( prevData ) => ({
         hashtags: prevData.hashtags,
         counts: {
-          tag1: Math.floor(Math.random() * 20), // 임시로 랜덤한 값 생성
-          tag2: Math.floor(Math.random() * 20),
-          tag3: Math.floor(Math.random() * 20),
+          tag1: Math.floor( Math.random() * 20 ), // 임시로 랜덤한 값 생성
+          tag2: Math.floor( Math.random() * 20 ),
+          tag3: Math.floor( Math.random() * 20 ),
+          tag4: Math.floor( Math.random() * 20 ),
+          tag5: Math.floor( Math.random() * 20 ),
+          tag6: Math.floor( Math.random() * 20 ),
         },
       }));
-    }, 1000);
+    }, 5000); //5초마다 갱신
 
     // 컴포넌트가 언마운트되면 interval 해제
     return () => clearInterval(intervalId);
@@ -76,10 +84,14 @@ const TemporaryParentComponent: React.FC = () => {
 
   return (
     <div>
-      <h1>Matching Queue Bubble Chart</h1>
-      <MatchingQueueBubbleChart data={matchingData} />
+      <div style={{ display:'flex', justifyContent:'space-between'}}>
+        <Title type='large'>직관 메이트를 찾고 있어요</Title>
+        <Timer duration={ 180 }/>
+      </div>
+      <MatchingQueue data={ matchingData }/>
+
     </div>
   );
 };
 
-export default TemporaryParentComponent;
+export default Matching;
