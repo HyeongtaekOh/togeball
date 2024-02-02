024a56d83133f91a11f098aaf24e5894fc2f1f2f
양유경 <gosjorb@gmail.com>
2024-02-02 10:21:55 +0900

    Feat : OAuth2 카카오, 구글 로그인 구현

---
+++ backend/src/main/java/com/ssafy/togeball/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ssafy.togeball.domain.user.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super();
+    }
+}
+
