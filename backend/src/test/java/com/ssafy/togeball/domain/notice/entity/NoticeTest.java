48baacc8e98bd3d53f130fe9bf6dcf403a60dd4b
조아영 <aycho3030@gmail.com>
2024-02-03 23:17:16 +0900

    feat: Notice API 작성 (테스트 필요)

---
+++ backend/src/test/java/com/ssafy/togeball/domain/notice/entity/NoticeTest.java
@@ -0,0 +1,13 @@
+package com.ssafy.togeball.domain.notice.entity;
+
+import org.junit.jupiter.api.Test;
+
+public class NoticeTest {
+
+    @Test
+    void noticeBuilderTest() {
+        //Given
+        //When
+        //Then
+    }
+}
