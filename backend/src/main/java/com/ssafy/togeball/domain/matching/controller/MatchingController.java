a8cd61f33394050ad452d5c51ec694fa7d69b317
HyeongtaekOh <oh052679@google.com>
2024-02-05 14:14:10 +0900

    Feat: Matching 조회/생성 API 추가

--- backend/src/main/java/com/ssafy/togeball/domain/matching/controller/MatchingController.java
+++ backend/src/main/java/com/ssafy/togeball/domain/matching/controller/MatchingController.java
@@ -1,9 +1,14 @@
 package com.ssafy.togeball.domain.matching.controller;
 
+import com.ssafy.togeball.domain.matching.dto.MatchingRequest;
+import com.ssafy.togeball.domain.matching.dto.MatchingResponse;
 import com.ssafy.togeball.domain.matching.service.MatchingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Map;
 
 @RestController
@@ -16,5 +21,22 @@ public class MatchingController {
     @GetMapping("/test")
     public Map<String, Object> test() {
         return Map.of("test", "test");
+    }
+
+    @GetMapping("/{matchingId}")
+    public ResponseEntity<?> getMatching(@PathVariable Integer matchingId) {
+        MatchingResponse matching = matchingService.findMatchingById(matchingId);
+        return ResponseEntity.ok(matching);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMatching(@RequestBody MatchingRequest matchingRequest) {
+        MatchingResponse matching = matchingService.createMatchingAndChatroom(matchingRequest);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{matchingId}")
+                .buildAndExpand(matching.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
