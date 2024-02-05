9add7a380e9d18f9f48559e5bf147496b2ac1ed0
HyeongtaekOh <oh052679@google.com>
2024-02-05 14:14:10 +0900

    Feat: Matching 조회/생성 API 추가

--- backend/src/main/java/com/ssafy/togeball/domain/matching/service/MatchingService.java
+++ backend/src/main/java/com/ssafy/togeball/domain/matching/service/MatchingService.java
@@ -1,6 +1,8 @@
 package com.ssafy.togeball.domain.matching.service;
 
+import com.ssafy.togeball.domain.matching.exception.MatchingNotFoundException;
 import com.ssafy.togeball.domain.matching.dto.MatchingRequest;
+import com.ssafy.togeball.domain.matching.dto.MatchingResponse;
 import com.ssafy.togeball.domain.matching.entity.Matching;
 import com.ssafy.togeball.domain.matching.repository.MatchingRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +17,20 @@ public class MatchingService {
 
     private final MatchingRepository matchingRepository;
 
+    @Transactional(readOnly = true)
+    public MatchingResponse findMatchingById(Integer matchingId) {
+        Matching matching = matchingRepository
+                .findById(matchingId)
+                .orElseThrow(MatchingNotFoundException::new);
+        return MatchingResponse.of(matching);
+    }
+
     /**
      * 매칭과 매칭 채팅방을 생성하고, 매칭에 참여하는 유저들을 추가한다.
      */
     @Transactional
-    public Matching createMatchingAndChatroom(MatchingRequest matchingDto) {
-        return matchingRepository.createMatchingAndChatroom(matchingDto);
+    public MatchingResponse createMatchingAndChatroom(MatchingRequest matchingDto) {
+        Matching matching = matchingRepository.createMatchingAndChatroom(matchingDto);
+        return MatchingResponse.of(matching);
     }
 }
