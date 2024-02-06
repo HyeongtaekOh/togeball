7e5b70057ef0ebc6e0474aa286f9c0420b7e9c50
양유경 <gosjorb@gmail.com>
2024-02-06 16:38:31 +0900

    cors 설정 add mapping 모든 것으로 변경

--- backend/src/main/java/com/ssafy/togeball/global/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/togeball/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
