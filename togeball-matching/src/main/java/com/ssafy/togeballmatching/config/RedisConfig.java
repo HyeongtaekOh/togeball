0dbec3f4a665aa371bd34818800436a338fa3507
HyeongtaekOh <oh052679@google.com>
2024-02-06 12:21:23 +0900

    "Feat: 매칭 웹소켓 연결 SockJS 전환 & Redis password 인증"

--- togeball-matching/src/main/java/com/ssafy/togeballmatching/config/RedisConfig.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/config/RedisConfig.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -25,9 +27,14 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
--- togeball-matching/src/main/java/com/ssafy/togeballmatching/config/WebSocketConfig.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(customWebSocketHandler, "/matching-server/matching")
                 .setAllowedOriginPatterns("*")
-                .addInterceptors(new AuthHandshakeInterceptor());
+                .withSockJS()
+                .setInterceptors(new AuthHandshakeInterceptor());
     }
 }
