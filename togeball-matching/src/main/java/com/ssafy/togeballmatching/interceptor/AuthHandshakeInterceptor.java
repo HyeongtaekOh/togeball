3ecce57016deda5846a676e584f63217a8806fb0
조아영 <aycho3030@gmail.com>
2024-02-07 14:40:30 +0900

    feat: notice service 테스트 코드 작성 완료

--- togeball-matching/src/main/java/com/ssafy/togeballmatching/interceptor/AuthHandshakeInterceptor.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/interceptor/AuthHandshakeInterceptor.java
@@ -33,19 +33,22 @@ public class AuthHandshakeInterceptor implements HandshakeInterceptor {
         // 2. 로그인한 유저라면 Headers의 Authorization에 bearerToken이 있을 것
 
         // 2-1. bearerToken을 받아오는 방법 (1)
-//        String bearerToken = request.getHeaders().get("Authorization").toString();
-//        String token;
-//        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-//            token = bearerToken.substring(7);
-//        }
+        String bearerToken = request.getHeaders().get("Authorization").toString();
+        String token;
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            token = bearerToken.substring(7);
+            log.info("token : {}", token);
+        } else {
+            log.info("왜 안 들어왔지");
+        }
 
         // 2-2. bearerToken을 받아오는 방법 (2) - 이렇게 하면 바로 복호화까지 되는 듯?
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String user;
-        if (authentication != null && authentication.getName() != null) user = authentication.getName();
-        else throw new RuntimeException("Authentication 정보가 없습니다.");
+//        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String user;
+//        if (authentication != null && authentication.getName() != null) user = authentication.getName();
+//        else throw new RuntimeException("Authentication 정보가 없습니다.");
 
-        attributes.put("userId", user); //좀 더 생각해보기
+//        attributes.put("userId", user); //좀 더 생각해보기
 
         /*
             Spring WebClient는 웹으로 API를 호출하기 위해 사용되는 Http Client 모듈 중 하나
@@ -58,17 +61,17 @@ public class AuthHandshakeInterceptor implements HandshakeInterceptor {
         // 정보 요청은 https://localhost:8080/webClientTest.html
         // (근데 그냥 accessToken을 바로 보내서 백에서 처리하게 하면 안 되나?)
 
-            WebClient webClient = WebConfig.getBaseUrl();
-            ResponseEntity<JSONObject> matchingUser = webClient.get()
-                    .uri("/api/users/" + user)
-                    .retrieve()
-                    .toEntity(JSONObject.class) //String.class로도 가능
-                    .block();
-            String email = matchingUser.getBody().get("email").toString();
-            log.info("statusCode : {}", matchingUser.getStatusCode());
-            log.info("header : {}", matchingUser.getHeaders()); //jwt token은 헤더에 담겨 오나...? 뭐라고 담겨 오지
-            log.info("body : {}", matchingUser.getBody());
-            log.info("email : {}", email);
+//            WebClient webClient = WebConfig.getBaseUrl();
+//            ResponseEntity<JSONObject> matchingUser = webClient.get()
+//                    .uri("/api/users/" + user)
+//                    .retrieve()
+//                    .toEntity(JSONObject.class) //String.class로도 가능
+//                    .block();
+//            String email = matchingUser.getBody().get("email").toString();
+//            log.info("statusCode : {}", matchingUser.getStatusCode());
+//            log.info("header : {}", matchingUser.getHeaders()); //jwt token은 헤더에 담겨 오나...? 뭐라고 담겨 오지
+//            log.info("body : {}", matchingUser.getBody());
+//            log.info("email : {}", email);
             //WebClient를 이용해 유저 정보 요청 끝
 
         try {
