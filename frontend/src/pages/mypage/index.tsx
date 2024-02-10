444a3b7562ed826aaeda46e71d9ff09ebfc4bdcc
조권호 <whrnsgjh12@naver.com>
2024-02-11 00:40:53 +0900

    Refactor: Mypage Ticket

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components'
-import { getUserInfo } from 'src/api'
+
 import { TagBtn, Title, HomeLayout, MainLayout, BoardItem, ChatItem } from 'src/components'
 import { useNavigate } from 'react-router-dom'
 import { Ticket } from './components'
@@ -11,101 +11,39 @@ const MainWrapper= styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    
 `
 
-const MyPage = ( () => {
-    
-    const { data: userInfo } = useQuery([ 'user' ], () => getUserInfo())
+const SubjectWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 24px;
+    font-weight: bolder;
+    margin-top: 20px;
+`
 
-//     https://i10a610.p.ssafy.io:8080/api/users/me
-// {
-//     "id": 2,
-//     "email": "ssafy@ssafy.com",
-//     "nickname": "hi1",
-//     "gender": null,
-//     "birthdate": null,
-//     "phone": null,
-//     "profileImage": null,
-//     "tags": [
-//         {
-//             "id": 1,
-//             "content": "LG",
-//             "type": "PREFERRED_TEAM"
-//         },
-//         {
-//             "id": 29,
-//             "content": "내야일반석",
-//             "type": "PREFERRED_SEAT"
-//         },
-//         {
-//             "id": 34,
-//             "content": "감독형",
-//             "type": "CHEERING_STYLE"
-//         },
-//         {
-//             "id": 38,
-//             "content": "ENFP",
-//             "type": "MBTI"
-//         },
-//         {
-//             "id": 60,
-//             "content": "2030대",
-//             "type": "UNLABELED"
-//         }
-//     ],
-//     "clubSponsorName": null,
-//     "clubName": null,
-//     "clubLogo": null
-// }
-    // const user = { id: userInfo.id, myTeam : userInfo.clubSponsorName+userInfo.clubName,  nickName: userInfo.nickname, tag: userInfo.tags, logo: userInfo.clubLogo }
-    const user = { id: 'test01', myTeam : 'LG 트윈스', stadium : '고척 돔', nickName: '플레권', tag: ['#Estp', '#응원가형', '#시즌권보유', '#자차보유','#플레권'], logo: ''}
+
+const MyPage = ( () => {
+ 
+
     const board = { creatorName:'이운재', title: 'LG', createdTime: '2021-09-20', logo:'https://avatars.githubusercontent.com/u/10000000?v'}
-    const chatItem =    {
-        "chatrooms" : [
-          {
-             "participants": [
-               {
-                  "userId": 'user1',
-                  "email": 'user1@example.com',
-                  "nickname": 'user1'
-               },
-             ],
-             "title": "타이틀2!!",
-             "tags": ["#LG", "#KT", "#SSG", "#NC", "#KIA",],
-             "manager": {
-                  "userId": 3,
-                  "email": 'tmp@gmail.com',
-                  "nickname": 'tmp'
-              },
-              "gameId": 23,
-              "capacity": 4,
-              "cheeringTeamImageUrl": "httpURL"
-              }
-          ],
-          "totalCount": 20,
-          "pageSize": 10,
-          "pageNo": 1
-      }
-   
+
     return (
         <MainLayout title='마이 페이지'>
+            <HomeLayout>
             <Ticket/>
-                <Title type='large'>
-                    내가 모집중인 채팅방
-                    </Title>
+            <SubjectWrapper>
+                내가 모집중인 채팅방
+            </SubjectWrapper>
             <MainWrapper>
                 {/* <ChatItem item={ chatItem }/> */}
-                </MainWrapper>
-                <div style={{ margin: '50px' }}></div>
-                <Title type='large'>
+            </MainWrapper>
+                <SubjectWrapper>
                     내가 쓴 게시글
-                </Title>
+                </SubjectWrapper>
             <MainWrapper>
-
                 <BoardItem board={ board }></BoardItem>
             </MainWrapper>
-
+        </HomeLayout>
     </MainLayout>
     )
 })
