ff6d54b0537e6df5a240e0c5fea9c4d7bd5fae0b
조아영 <aycho3030@gmail.com>
2024-02-12 15:45:34 +0900

    refactor: matching api

--- togeball-matching/src/main/java/com/ssafy/togeballmatching/scheduler/MatchingScheduler.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/scheduler/MatchingScheduler.java
@@ -56,21 +56,20 @@ public class MatchingScheduler {
             // 2-1. 유저 목록 받아옴
             List<Integer> userIds = sessions.stream().map(session -> (Integer) session.getAttributes().get("userId")).toList();
 
-            //TODO: 그냥 매칭 생략하고 현재 대기 중인 사용자 전체를 한번에 매칭시켜도 좋을 듯? 테스트 인원도 소규모일 예정이니
-
             // 2-2. 유저별 태그 받을 Set과 List
             List<List<Tag>> tags = new ArrayList<>();
 
             // 2-3. 태그별 빈도수 카운트할 HashMap
-            Map<String, Integer> userTags = new HashMap<>();
+            Map<Integer, Integer> userTags = new HashMap<>(); //태그 번호, 빈도수
 
             // 2-4. 태그 받아옴
             for (int i=0; i<userIds.size(); i++) {
                 List<Tag> tag = (ArrayList) sessions.get(i).getAttributes().get("tags");
                 for (int j=0; j<tag.size(); j++) {
-                    String content = tag.get(i).getContent();
-                    if (userTags.containsKey(content)) userTags.put(content,userTags.get(content)+1);
-                    else userTags.put(content, 0);
+//                    String content = tag.get(i).getContent();
+                    int id = tag.get(i).getId();
+                    if (userTags.containsKey(id)) userTags.put(id,userTags.get(id)+1);
+                    else userTags.put(id, 0);
                 }
                 tags.add(tag);
             }
@@ -79,13 +78,14 @@ public class MatchingScheduler {
             log.info("userTags(0):{}",tags.get(0).get(0).getType());
 
             // 2-5. 태그별 빈도수로 내림차순 정렬
-            List<String> keySet = new ArrayList<>(userTags.keySet());
+            List<Integer> keySet = new ArrayList<>(userTags.keySet());
             keySet.sort((o1,o2) -> userTags.get(o2).compareTo(userTags.get(o1))); //value값으로 내림차순 정렬
 
-            // 2-6. 빈도수 많은 순으로 최대 10개 출력
-            int tagCount = keySet.size()>=10 ? 10 : keySet.size();
-            for (int i=0; i<tagCount; i++) {
-                String key = keySet.get(i);
+            // 2-6. 빈도수 많은 순으로 태그 리스트에 담음
+            List<Integer> sortedTagIds = new ArrayList<>(); //담을 리스트
+            for (int i=0; i<keySet.size(); i++) {
+                int key = keySet.get(i);
+                sortedTagIds.add(key); //리스트에 담음
                 System.out.print("Key: " + key); //태그명
                 System.out.println(", Val: " + userTags.get(key)); //빈도수
             }
@@ -96,7 +96,7 @@ public class MatchingScheduler {
                     .title(LocalDateTime.now().toString()+"에 이루어진 매칭입니다.")
                     .capacity(userIds.size())
                     .userIds(userIds)
-                    .tagIds(List.of(1,2,3))  // TODO: 매칭된 유저의 태그 ID 목록을 가져와 설정한다. (중복 제거 필수)
+                    .tagIds(sortedTagIds) // TODO: 매칭된 유저의 태그 ID 목록을 가져와 설정한다. (중복 제거 필수)
                     .build();
             rabbitService.sendMessage(exchange, routingKey, matchingRequest);
             messagingService.sendMatchingResultToUsers(userIds.subList(0, 2));
