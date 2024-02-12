e8a7917de8fe514bb118d605b0f892c5e2f78630
오형택 <oh052679@gmail.com>
2024-02-12 15:59:15 +0900

    HotFix: 채팅 웹소켓 Handshake 오류 재수정

--- togeball-chat/src/main/java/com/ssafy/togeballchatting/interceptor/AuthHandshakeInterceptor.java
+++ togeball-chat/src/main/java/com/ssafy/togeballchatting/interceptor/AuthHandshakeInterceptor.java
@@ -2,12 +2,15 @@ package com.ssafy.togeballchatting.interceptor;
 
 import com.ssafy.togeballchatting.service.JwtService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class AuthHandshakeInterceptor implements HandshakeInterceptor {
@@ -19,14 +22,18 @@ public class AuthHandshakeInterceptor implements HandshakeInterceptor {
 
         log.info("beforeHandshake");
         String query = request.getURI().getQuery();
-        String token = query.split("=")[1];
+        Matcher matcher = authMatcher(query);
 
-        log.info("token: {}", token);
-        return jwtService.isTokenValid(token);
+        return matcher.find() && jwtService.isTokenValid(matcher.group(1));
     }
 
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
 
+    }
+    public Matcher authMatcher(String query) {
+        String pattern = "Authorization([^&]+)";
+        Pattern r = Pattern.compile(pattern);
+        return r.matcher(query);
     }
 }
