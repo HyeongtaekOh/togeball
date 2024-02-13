e9411d186d1e756c3813525a1aed2b1420bd5b69
조아영 <aycho3030@gmail.com>
2024-02-13 16:53:35 +0900

    .

--- togeball-matching/src/main/java/com/ssafy/togeballmatching/interceptor/AuthHandshakeInterceptor.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/interceptor/AuthHandshakeInterceptor.java
@@ -4,9 +4,12 @@ import com.ssafy.togeballmatching.config.WebConfig;
 import com.ssafy.togeballmatching.dto.MatchingUser;
 import com.ssafy.togeballmatching.dto.Tag;
 import com.ssafy.togeballmatching.service.queue.RedisWaitingQueueService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -22,6 +25,7 @@ import java.util.regex.Pattern;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthHandshakeInterceptor implements HandshakeInterceptor {
 
     private RedisWaitingQueueService redisWaitingQueueService;
@@ -42,29 +46,28 @@ public class AuthHandshakeInterceptor implements HandshakeInterceptor {
         }
         String accessToken = matcher.group(1);
 
-//        String accessToken = request.getURI().getQuery().split("token=")[1];
-//        log.info("accessToken:{}",accessToken);
-
         String[] parts = accessToken.split("\\."); // JWT 토큰 파싱
         byte[] decodedBytes = Base64.getUrlDecoder().decode(parts[1]); // 디코딩
         String decodedPayload = new String(decodedBytes);
-
         JSONParser parser = new JSONParser();
         JSONObject payload = (JSONObject) parser.parse(decodedPayload);
         String id = payload.get("id").toString(); //유저 id
         Integer userId = Integer.parseInt(id);
-
         attributes.put("userId", userId);
 
         WebClient webClient = WebConfig.getBaseUrl(); // token에서 추출한 userId로 WebClient를 이용해 유저 정보를 요청
         MatchingUser user = webClient.get()
                 .uri("/api/users/" + userId)
+//                .uri("/api/users/me")
+//                .header(HttpHeaders.AUTHORIZATION, "Bearer "+accessToken)
                 .retrieve()
                 .bodyToMono(MatchingUser.class)
                 .block();
         log.info("{},{},{},{}",user.getUserId(),user.getNickname(),user.getProfileImage(),user.getTags());
 //        log.info("{}",user.getTags().get(0).getType()); //PREFERRED_TEAM
 //        boolean temp = user.getTags().get(0) instanceof Tag; //true
+
+        attributes.put("userId", user.getUserId());
         attributes.put("tags", user.getTags());
 
         redisWaitingQueueService.addQueue(user); // 대기열 큐에 유저를 저장
