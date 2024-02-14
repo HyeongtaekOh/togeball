3e977401dd2f8af2fdbb2d8b1fab5f7de36e8c01
양유경 <gosjorb@gmail.com>
2024-02-14 10:05:43 +0900

    Feat: User ADMIN role 추가

--- backend/src/main/java/com/ssafy/togeball/domain/user/entity/Role.java
+++ backend/src/main/java/com/ssafy/togeball/domain/user/entity/Role.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
 
     // guest : 프로필 설정 전 회원, basic : 회원, verified : 본인 인증 완료 회원
-    GUEST("ROLE_GUEST"), BASIC("ROLE_BASIC"), VERIFIED("ROLE_VERIFIED");
+    GUEST("ROLE_GUEST"), BASIC("ROLE_BASIC"), VERIFIED("ROLE_VERIFIED"), ADMIN("ROLE_ADMIN");
 
     private final String key;
 }
