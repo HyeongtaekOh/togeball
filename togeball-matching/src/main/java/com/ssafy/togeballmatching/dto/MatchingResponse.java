c1844584343279ba47536a8415abe00d884759a1
조아영 <aycho3030@gmail.com>
2024-02-14 14:35:11 +0900

    fix: matching scheduler

--- togeball-matching/src/main/java/com/ssafy/togeballmatching/dto/MatchingResponse.java
+++ togeball-matching/src/main/java/com/ssafy/togeballmatching/dto/MatchingResponse.java
@@ -1,7 +1,14 @@
 package com.ssafy.togeballmatching.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
 public class MatchingResponse {
 
     private Integer id;
@@ -10,5 +17,4 @@ public class MatchingResponse {
     private Integer capacity;
     private List<UserResponse> users;
     private List<TagResponse> tags;
-
 }
