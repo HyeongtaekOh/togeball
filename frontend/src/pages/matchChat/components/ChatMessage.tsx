f436e584efe8a96fa4f390267157b202cd95519a
윤진아 <chris2761@naver.com>
2024-02-15 21:27:23 +0900

    Fix : chat css

--- frontend/src/pages/matchChat/components/ChatMessage.tsx
+++ frontend/src/pages/matchChat/components/ChatMessage.tsx
@@ -2,9 +2,11 @@ import { getUserInfo } from 'src/api'
 import { useEffect, useState } from 'react'
 import { Title } from 'src/components'
 import styled, { css } from 'styled-components'
+import Logo from 'src/asset/images/Logo.jpg'
 
 const ChatMessageWrapper = styled.div<{ isMe?: string }>` 
   display: flex;
+  min-width: 300px;
   justify-content: ${ props => props.isMe !== 'you' ? 'flex-end' : 'flex-start' }; 
 `
 const ChatWrapper = styled.div<{ isMe?: string}>`
@@ -15,6 +17,7 @@ const ChatWrapper = styled.div<{ isMe?: string}>`
   flex-direction: column;
   margin-top : 1px;
   padding: 7px;
+  margin: 0px 10px;
 
   ${( props ) =>
     props.isMe ==='you' &&
@@ -22,9 +25,8 @@ const ChatWrapper = styled.div<{ isMe?: string}>`
     background-color: #FBD14B;
   `}
 `
-const ChatTopWrapper = styled.div<{ isMe? : string }>`
-  min-width: 150px;
-  max-width: 50%;
+const ChatTopWrapper = styled.div<{ isMe?: string }>`
+  width: 30%;
   display: flex;
   flex-direction: column;
   padding: 5px;
@@ -38,8 +40,10 @@ const TimeWrapper = styled.p<{ isMe? : string }>`
   text-align: ${(props)=>props.isMe === 'me'? 'right': 'left' }
 `
 const NickWrapper = styled.p<{ isMe? : string }>`
-  align-self: ${(props)=>props.isMe === 'me'? 'flex-end': 'flex-start' };
-  font-size: 10px;
+  display: flex;
+  font-size: 11px;
+  align-items: center;
+  font-weight: bold;
 `
 const ImgWrapper = styled.img`
   width: 20px;
@@ -69,23 +73,32 @@ const ChatMessage = ( props: ChatMessageProps ) => {
     <ChatMessageWrapper isMe = { isMe }> 
     {
       senderId? (
-        <div style={{ width: '100%', height:'100%', display: 'flex'}}>
-          <ImgWrapper src = {userImage} alt="hi"/>
-          <ChatTopWrapper isMe = { isMe }>
+        <ChatTopWrapper isMe = { isMe }>
             <div style={{ display:'flex', flexDirection: 'row', gap: '5px' }}>
               { isMe === 'me' &&  <TimeWrapper isMe={ isMe }>{ time.substring(0, 8) }</TimeWrapper>}
               <div style={{ display:'flex', flexDirection: 'column', gap : '5px', width:'100%' }}>
-                <NickWrapper isMe = { isMe }>{ nickname } </NickWrapper>
-                {
-                  type === 'IMAGE'?
-                  ( <ChatWrapper isMe = { isMe }><img src={ content } style={{ width: '100%', height: '100%' }} alt=''/></ChatWrapper> ):
-                  ( <ChatWrapper isMe = { isMe }>{ content }</ChatWrapper> )
-                }
+              {
+              ( isMe==='me' )?(
+               <div style={{ display:'flex', width: '100%', justifyContent: 'flex-end', gap: '5px'}}>
+               <NickWrapper>{ nickname } </NickWrapper>
+               <ImgWrapper src = { userImage || Logo } alt="hi"/>
+               </div>
+              ):(
+               <div style={{ display:'flex', width: '100%', justifyContent: 'flex-start', gap: '5px' }}>
+               <ImgWrapper src = { userImage || Logo } alt="hi"/>
+               <NickWrapper>{ nickname } </NickWrapper>
+               </div>
+              )
+            }
+            {
+              type === 'IMAGE'?
+              ( <ChatWrapper isMe = { isMe }><img src={ content } style={{ width: '100%', height: '100%' }} alt=''/></ChatWrapper> ):
+              ( <ChatWrapper isMe = { isMe }>{ content }</ChatWrapper> )
+            }
               </div>  
               { isMe === 'you' && <TimeWrapper isMe={ isMe }>{ time.substring(0, 8) }</TimeWrapper>}
             </div>  
           </ChatTopWrapper>
-        </div>
       ):( 
         <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
           <Title type='small'>{ content }</Title> 
