09f22b5e22d78a1a84d6d1478723be4345cf4403
윤진아 <chris2761@naver.com>
2024-02-15 12:33:28 +0900

    Fix : add isLoading and updateSelectTags

--- frontend/src/pages/profile/store/index.ts
+++ frontend/src/pages/profile/store/index.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand'
 
 const useModel = create<Model>( ( set, get ) => (
   {
+
     selectTags: [],
     addSelectTags: (tag) => set((state) => {
       const isDuplicate = state.selectTags.some(item => item.id === tag.id);
@@ -12,20 +13,21 @@ const useModel = create<Model>( ( set, get ) => (
           };
       }
       return state;
-  }),
+    }),
     deleteTags: ( tag ) => set(( state ) => (
       {
         selectTags: 
           state.selectTags.filter(( item ) => item.content !== tag.content )
       }
     )),
-    resetTags: () => set(() =>({
-      selectTags:
-      []
+    updateTags: ( list ) => set(() =>({
+      selectTags: list
     }
     )),
+
     team: 0,
     setTeam: ( data ) => set(() => ({ team: data })),
+
     stadiums: [],
     addStadiums: (stadium) => set((state) => {
       const isDuplicate = state.stadiums.some(item => item.id === stadium.id);
@@ -35,38 +37,49 @@ const useModel = create<Model>( ( set, get ) => (
           };
       }
       return state;
-  }),
+    }),
     deleteStadiums: ( stadium ) => set(( state ) => (
       {
         stadiums: 
           state.stadiums.filter(( item ) => item !== stadium )
       }
     )),
+
     image: '',
     setImage: ( data ) => set(() => ({ image: data })),
+
     gender: '',
     setGender: ( data ) => set(()=>({ gender: data })),
+
     selectedDate: '',
     setSelectedDate: ( data ) => set(() => ({ selectedDate: data }))
+
   }
 ))
 
 export default useModel
 
 export interface Model{
+
   selectTags : TagType[]
   addSelectTags : ( tag: TagType ) => void
   deleteTags : ( tag: TagType ) => void
-  resetTags : () => void
+  updateTags : ( list: TagType[] ) => void
+
   team: number
   setTeam: ( data: number ) => void
+
   stadiums: TagType[]
   addStadiums: ( stadium: TagType ) => void
   deleteStadiums: ( stadium: TagType ) => void
+
   image: string
   setImage: ( data ) => void
+
   gender: string
   setGender: ( data ) => void 
+
   selectedDate: string
   setSelectedDate: ( data ) => void
+
 }
